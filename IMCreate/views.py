d84697197fd2e41813ad8fb5e2f87d80d8982c27
A-tenBensel <atenbensel@unomaha.edu>
2026-04-01 22:20:21 -0500

    Migrate Post views
    * Update views.py
    
    * Update index.html
    
    ---------
    
    Co-authored-by: Aaron Schmitz

--- IMCreate/views.py
+++ IMCreate/views.py
@@ -1,7 +1,17 @@
 from django.contrib.auth import login
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
-from apps.posts.models import Post
+from .models import Post
 
 def front_page(request):
-  return render(request, "index.html", {"posts": Post.objects.all})
+  tags = request.GET.get("tags","").strip()
+  if request.method == "POST":
+    tags = request.POST.get("tags","").strip()
+    tagsList = tags.split(',')
+    for i in range(len(tagsList)):
+      tagsList[i] = tagsList[i].strip()
+  if tagsList:
+    posts = Post.objects.filter(tags__name__in=tagsList).order_by("upload_date")
+  else:
+    posts = Post.objects.all().order_by("upload_date")
+  return render(request,'index.html',{'posts':posts, 'tags':tags})
