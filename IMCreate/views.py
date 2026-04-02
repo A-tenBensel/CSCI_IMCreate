2747653caf8b0c09dae5bf984a7adb8b463d7b3d
DF417 <167568295+DF417@users.noreply.github.com>
2026-04-02 01:48:47 -0500

    Fixed tag search
    improved tag search logic

--- IMCreate/views.py
+++ IMCreate/views.py
@@ -4,15 +4,15 @@ from django.shortcuts import render, redirect
 from apps.posts.models import Post
 
 def front_page(request):
-  if request.method == "POST":
-    tags = request.POST.get("tags","").strip()
+  tags = request.GET.get("tags","").strip()
+
+  if tags:
+    tagsList = [t.strip() for t in tags.split(',') if t.strip()]
+    posts = Post.objects.filter(tags__name__in=tagsList).distinct()
+
   else:
-    tags = request.GET.get("tags","").strip()
-  tagsList = tags.split(',')
-  for i in range(len(tagsList)):
-    tagsList[i] = tagsList[i].strip()
-  if tagsList != ['']:
-    posts = Post.objects.filter(tags__name__in=tagsList).order_by("upload_date")
-  else:
-    posts = Post.objects.all().order_by("upload_date")
+    posts = Post.objects.all()
+  
+  posts = posts.order_by("upload_date")
+  
   return render(request,'index.html',{'posts':posts, "tags_query": tags})
