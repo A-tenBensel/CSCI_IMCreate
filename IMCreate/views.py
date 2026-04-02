69cb66e9fb85fe5b4091d10f8a1f129189ff2f30
A-tenBensel <atenbensel@unomaha.edu>
2026-04-02 00:04:46 -0500

    Reorganize post views logic
    Allow post tags

--- IMCreate/views.py
+++ IMCreate/views.py
@@ -1,17 +1,18 @@
 from django.contrib.auth import login
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
-from .models import Post
+from apps.posts.models import Post
 
 def front_page(request):
-  tags = request.GET.get("tags","").strip()
   if request.method == "POST":
     tags = request.POST.get("tags","").strip()
-    tagsList = tags.split(',')
-    for i in range(len(tagsList)):
-      tagsList[i] = tagsList[i].strip()
-  if tagsList:
+  else:
+    tags = request.GET.get("tags","").strip()
+  tagsList = tags.split(',')
+  for i in range(len(tagsList)):
+    tagsList[i] = tagsList[i].strip()
+  if tagsList != ['']:
     posts = Post.objects.filter(tags__name__in=tagsList).order_by("upload_date")
   else:
     posts = Post.objects.all().order_by("upload_date")
-  return render(request,'index.html',{'posts':posts, 'tags':tags})
+  return render(request,'index.html',{'posts':posts, "tags_query": tags})
