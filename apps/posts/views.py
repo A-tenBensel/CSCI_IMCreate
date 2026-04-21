35ca49062059358d1c2cc08560c82933a490a3f8
A-tenBensel <atenbensel@unomaha.edu>
2026-04-20 23:47:03 -0500

    implement comments

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from IMCreate.render import RenderPostForm
 from apps.posts.models import Post
 from apps.social.models import Follower
+from IMCreate.forms.forms import CommentForm
 
 def make_post(request):
   return RenderPostForm(request,view='make_post.html').render
@@ -13,3 +15,18 @@ def view_post(request, post_id):
   else:
     is_following = False
   return render(request, 'components/post.html',{'post': post, 'is_following': is_following})
+
+def comment(request, post_id):
+  post = Post.objects.get(id=post_id)
+  if not request.user.is_authenticated:
+    return redirect("view_post", post_id = post.id)
+  if request.method == "POST":
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+      comment = comment_form.save(commit=False)
+      comment.user = request.user
+      comment.post = post
+      comment.save()
+  else:
+    comment_form = CommentForm()
+  return render(request, 'components/post.html', {'post': post, "comment_form": comment_form})
